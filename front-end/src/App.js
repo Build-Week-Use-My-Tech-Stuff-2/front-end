5589116119ede1db11cc0588f0b5c7266211d0a4
Alex <schroedergoncalves@gmail.com>
2020-07-27 14:10:59 -0400

    Buyer and Seller Components, Router architecture mockup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import BuyerSignup from './Components/BuyerSignup'
+import RenterSignUp from './Components/RenterSignUp'
 
 function App() {
   return (
     <div className="App">
-      <h1>Use My Tech APP</h1>
+        <h1> WareShare </h1>
+
+        <nav>
+          <Link  to="/">Home</Link>
+          <Link to="/renter-signup">Rent Hardware</Link>
+          <Link to="/buyer-signup">Share Your Goods</Link>
+        </nav>
+        {/* Scrolling item gallery? */}
+        <Switch>
+          <Route path="/buyer-signup">
+            <BuyerSignUp />
+          </Route>
+          <Route path="/renter-signup">
+            <RenterSignUp />
+          </Route>
+          <Route path="/">
+
+          </Route>
+        </Switch>
     </div>
   );
 }
