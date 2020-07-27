77b1d2363d8ba290c0b2ac2700a80a96bb4a1b75
Ryan Lee <therealsassyfatcat@outlook.com>
2020-07-27 14:28:29 -0500

    saving

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
-import BuyerSignup from './Components/BuyerSignup'
+import BuyerSignUp from './Components/BuyerSignUp'
 import RenterSignUp from './Components/RenterSignUp'
 
 function App() {
+//////////// INITIAL STATES ////////////
+const initialForm = {
+  username: '',
+  password: ''
+};
+
+const initialFormErrors = {
+  username: '',
+  password: ''
+};
+
+//////////// INITIAL STATES ////////////
+const [forms, setForms] = useState(initialForm);
+const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+//////////// FORM ACTIONS ////////////
+const inputChange = (name, value) => {
+  setForms({
+    ...forms,
+    [name]: value
+  })
+}
+
   return (
     <div className="App">
         <h1> WareShare </h1>
@@ -17,10 +40,10 @@ function App() {
         {/* Scrolling item gallery? */}
         <Switch>
           <Route path="/buyer-signup">
-            <BuyerSignUp />
+            <BuyerSignUp inputChange={inputChange} forms={forms}/>
           </Route>
           <Route path="/renter-signup">
-            <RenterSignUp />
+            <RenterSignUp inputChange={inputChange} forms={forms}/>
           </Route>
           <Route path="/">
 
--- front-end/src/Components/BuyerSignup.js
+++ front-end/src/Components/BuyerSignup.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
 export default function BuyerSignup() {
+//////////// PROPS ////////////
+const {
+    inputChange
+} = props;
+
+//////////// HELPER FUNCTIONS ////////////
+const onChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    inputChange(name, value)
+}
+
     return (
         <div>
             
