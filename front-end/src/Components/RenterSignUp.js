5b20241f68d495ce60acd4a1fe4f712b2e171e6e
Ryan Lee <therealsassyfatcat@outlook.com>
2020-07-27 15:12:21 -0500

    saving

--- front-end/src/Components/RenterSignUp.js
+++ front-end/src/Components/RenterSignUp.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 
-export default function SellerSignup() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export default function SellerSignup(props) {
+    const history = useHistory();
+    //////////// PROPS ////////////
+    const {
+        inputChange,
+        forms
+    } = props;
+    
+    //////////// HELPER FUNCTIONS ////////////
+    const onChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+    
+        inputChange(name, value)
+    };
+    
+    const onSubmit = event => {
+        event.preventDefault();
+        history.push('/')
+    }
+    
+        return (
+            <div>
+                <h1>Seller Sign-up</h1>
+                <label>Username:
+                    <input
+                        type="text"
+                        name="username"
+                        value={forms.username}
+                        onChange={onChange}
+                    ></input>
+                </label>
+    
+                <label>Password:
+                    <input
+                        type="password"
+                        name="password"
+                        value={forms.password}
+                        onChange={onChange}
+                    ></input>
+                </label>
+    
+                <button onClick={onSubmit}>Submit</button>
+            </div>
+        )
+    }
+    
