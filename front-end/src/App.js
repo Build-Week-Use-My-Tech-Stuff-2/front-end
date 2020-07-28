fe07373d8f699d94eb7f9a22c727e0f77ab9607c
Oscar Lopez <48202254+Oscar-Lopez-Projects@users.noreply.github.com>
2020-07-28 13:30:03 -0700

    forms

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import CardForm from './renter/CardForm';
+import {MainData} from './renter/context/MainData';
+import CreateForm from './renter/CreateForm';
+import RenterLogin from './renter/RenterLogin';
+
 function App() {
+
+  
   return (
     <div className="App">
       <h1>Use My Tech APP</h1>
+      {/*<Router>
+        <Switch>
+            <Route exact path='/'>
+                <CreateForm />
+            </Route>
+            <Route exact path='/renterlogin'>
+                <RenterLogin />
+            </Route>
+            <Route exact path="/cardform">
+                <CardForm />
+            </Route>
+        </Switch>
+      </Router>
+    */}
+      
       
     </div>
   );
--- front-end/src/renter/CardForm.js
+++ front-end/src/renter/CardForm.js
@@ -1,39 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import {Link} from 'react-router-dom'
 
 const CardForm = ()=>{
+
     const InputStyled= styled.input`
-        display:flex;
-        text-align:center;
-        margin-bottom:10px;
-        padding:10px;
-    `
+    display:flex;
+    text-align:center;
+    margin-bottom:10px;
+    padding:10px;
+`
+
+    const initialItem ={
+        itemname:'',
+        itemdesc:'',
+        itemlocat:'',
+        itemrate:'',
+        itemimg: ''
+    }
+    {/*this is where the dummy data lives */}
+    const [items, setItems]= useState([]);
+
+    const [newItems,setNewItems]=useState(initialItem);
+
+    {/*useEffect to get /items */}
+
+    const handleChange = e =>{
+        setNewItems({...newItems,[e.target.name]: e.target.value })
+    }
+
+    {/*add new item using axios*/}
+
     return(
         <div>
             <h1>Rent Your item!</h1>
             <form style={{display:'flex', flexDirection:'column', width:'400px', margin:'0 auto'}}>
                 <InputStyled
                     placeholder='Item Name'
-                />
-                <InputStyled 
-                    placeholder='Type'
+                    onChange={handleChange}
+                    value={newItems.itemname}
+                    name="itemname"
+                    type="text"
                 />
                 <InputStyled 
                     placeholder='description'
+                    onChange={handleChange}
+                    value={newItems.itemdesc}
+                    name="itemdesc"
+                    type="text"
                 />
                 <InputStyled 
-                    placeholder='price per day'
+                    placeholder='price per hour'
+                    onChange={handleChange}
+                    value={newItems.itemrate}
+                    name="itemrate"
+                    type="number"
                 />
                 <InputStyled 
-                    placeholder='Duration'
+                    placeholder='location'
+                    onChange={handleChange}
+                    value={newItems.itemlocat}
+                    name="itemlocat"
+                    type="text"
                 />
-                <InputStyled
-                    placeholder='Pickup Location'
+                <InputStyled 
+                    placeholder='image link'
+                    onChange={handleChange}
+                    value={newItems.itemimg}
+                    name="itemimg"
+                    type="url"
                 />
-
                 <button style={{width:'100px', margin:'0 auto'}}>Add Item</button>
-
             </form>
+            <div>
+                Where the cardlist will be
+            </div> 
+            <Link to="/renterlogin">signout</Link>
         </div>
     )
 }
