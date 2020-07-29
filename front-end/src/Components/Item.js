19c7996f662b6befc73d70c7ae6980d593f48da1
Alex <schroedergoncalves@gmail.com>
2020-07-29 18:31:41 -0400

    Item reset functionality

--- front-end/src/Components/Item.js
+++ front-end/src/Components/Item.js
@@ -6,6 +6,22 @@ display: flex;
 flex-direction: column;
 background-color: ivory;
 max-width: 15%;
+height: 125px;
+width: 125px;
+margin: 1%;
+
+    img{
+        height: 70%;
+         width: 100%;
+    }
+
+    h3{
+        height: 10%;
+    }
+
+    h4{ 
+        height: 10%;
+    }
 `;
 
 export default function Item({item}) {
--- front-end/src/Components/Shop.js
+++ front-end/src/Components/Shop.js
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Styled from 'styled-components'
 
 import Item from './Item'
 import SearchBar from './SearchBar'
+
 
 const initItems = [
 
 ]
 
 const initSearch = ""
+
+const StyledGallery = Styled.section`
+  display: flex;
+  flex-flow: row wrap;
+  width: 80%;
+  margin: 5% auto 0 auto;
+  border: 2px solid #00A699;
+  background-color: rgba(72,72,72, .5);
+  justify-content: space-between;
+`
 
 export default function Shop() {
     const [items, setItems] = useState(initItems)
@@ -31,14 +43,14 @@ const loadItems = () => {
   )
     return (
         <div>
-           <SearchBar search={search} setItems={setItems} setSearch={setSearch}/>
-            <section id="gallery">
+           <SearchBar search={search} reset={loadItems} setItems={setItems} setSearch={setSearch}/>
+            <StyledGallery id="gallery">
                 {
                 items.map( item => {
                     return <Item item={item}/>
                 } )
                 }
-            </section> 
+            </StyledGallery> 
         </div>
     )
 }
