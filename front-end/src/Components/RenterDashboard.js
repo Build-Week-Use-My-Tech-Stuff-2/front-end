0ed1bffefe93386a5e4f9a918e81e24b463d4d3b
Alex <schroedergoncalves@gmail.com>
2020-07-29 13:47:51 -0400

    search bar component

--- front-end/src/Components/RenterDashboard.js
+++ front-end/src/Components/RenterDashboard.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { NavLink, Route } from 'react-router-dom'
+import Shop from './Shop'
 
 export default function RenterDashboard() {
     return (
         <div>
-            {/* nav */}
-            {/* Upgrade to seller */}
-            {/* Orders */}
-            {/* Gallery */}
+            <nav>
+                <NavLink to="/dashboard/shop">Shop</NavLink>
+
+                {/* rentals */}
+                {/* Upgrade*/}
+                {/* settings */}
+            </nav>
+
+            <Route path="/dashboard/shop"><Shop/></Route>
         </div>
     )
 }
